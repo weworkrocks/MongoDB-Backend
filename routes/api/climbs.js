5a63283cf970b25fa79970a1aa0996dced1671f1
kohdz <umair.stem@gmail.com>
2019-08-12 15:22:49 -0400

    Added Average Route

--- routes/api/climbs.js
+++ routes/api/climbs.js
@@ -23,6 +23,7 @@ router.post('/',
     }
 
     try {
+
     const user = await User.findById(req.user.id).select('-password');
 
     const newClimb = new Climb({
@@ -42,6 +43,78 @@ router.post('/',
         res.status(500).send('Server Error')
     }
 })
+
+
+//@route     Get api/climbs  Gets ALl CLimbs
+//@desc      GET all
+//@access    Private
+
+router.get('/', auth,  async (req, res) => {
+    try{
+        const climbs = await Climb.find().sort({ date: -1});
+        res.json(climbs)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+});
+
+
+//@route     Get api/climbs/:user  Gets Single CLimb
+//@desc      GET all
+//@access    Private
+router.get('/:user', auth,  async (req, res) => {
+    try {
+        console.log
+        // const climb = await Climb.findById(req.user);
+        // console.log('test', req.user)
+        // const climb = await Climb.findById(req.user)
+        // const climb = await Climb.findById(req.params.user)
+        const climb = await Climb.find({'user':req.user.id})
+        
+
+        if(!climb){
+            return res.status(404).json({msg: 'Climb Not Found 1'});
+        }
+        
+        res.json(climb)
+    } catch (err) {
+        if(err.kind === 'ObjectId'){
+            return res.status(404).json({msg: 'CLimb Not Found 2'})
+        }
+        res.status(500).send('Server Error')
+    }
+});
+
+//@route     Get api/climbs/:user/:averages
+//@desc      GET all
+//@access    Private
+router.get('/score/:usersum', auth,  async (req, res) => {
+    try {
+    
+        const climb = await Climb.find({'user':req.user.id})
+        sum = 0;
+        counter = 0;
+        climb.map( socre => {
+            sum += socre.climbScore;
+            counter +=1;
+        })
+        climbAverage = Math.round(sum/counter);
+
+
+        if(!climb){
+            return res.status(404).json({msg: 'Climb Not Found 1'});
+        }
+        
+        res.json(climbAverage)
+    } catch (err) {
+        if(err.kind === 'ObjectId'){
+            return res.status(404).json({msg: 'CLimb Not Found 2'})
+        }
+        res.status(500).send('Server Error')
+    }
+});
+
 
 
 module.exports = router;
